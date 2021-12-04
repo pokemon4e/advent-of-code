58a6964fd86ae1a56bf50e83058406064a4b1ba3
Milena Sapunova <milena.sapunova@gmail.com>
2021-12-04 23:21:21 +0000

    Add solution to 2021 Day 2

---
+++ 2021/day2/part1.py
@@ -0,0 +1,26 @@
+def solution(directios):
+    horizontal = 0
+    depth = 0
+    for d in directios:
+        direction = d[0]
+        units = int(d[1])
+        
+        match direction:
+            case 'forward':
+                horizontal += units
+            case 'up':
+                depth -= units
+            case 'down':
+                depth += units
+                
+    return horizontal * depth
+    
+def read_input(filename):
+    with open(filename) as f:
+        return [l.split() for l in f.readlines()]
+    
+def main():
+    print(solution(read_input('input.txt')))
+
+if __name__ == "__main__":
+    main()
